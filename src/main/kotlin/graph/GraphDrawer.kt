56afff0cfd0122aa697d17dbc70f8f86d291a487
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-11-21 23:43:44 +0300

    draw line

--- src/main/kotlin/graph/GraphDrawer.kt
+++ src/main/kotlin/graph/GraphDrawer.kt
@@ -1,21 +1,18 @@
 package graph
 
-import java.awt.Color
-
 import org.jfree.chart.ChartFactory
 import org.jfree.chart.ChartPanel
 import org.jfree.chart.JFreeChart
 import org.jfree.chart.axis.NumberAxis
-import org.jfree.chart.axis.NumberTickUnit
 import org.jfree.chart.plot.PlotOrientation
-import org.jfree.chart.plot.XYPlot
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer
 import org.jfree.data.xy.XYDataset
 import org.jfree.data.xy.XYSeries
 import org.jfree.data.xy.XYSeriesCollection
 import org.jfree.ui.ApplicationFrame
 import org.jfree.ui.RefineryUtilities
-import org.jfree.util.ShapeUtilities
+import java.awt.Color
+import java.awt.GradientPaint
 import java.awt.geom.Ellipse2D
 
 /**
@@ -23,30 +20,25 @@ import java.awt.geom.Ellipse2D
  * [XYDataset].
 
  */
-class GraphDrawer(title: String, data: List<Pair<Double,Double>>) : ApplicationFrame(title) {
+class GraphDrawer(title: String, data: List<DatasetHolder>) : ApplicationFrame(title) {
 
 	init {
-		val dataset = createDataset(data)
-		val chart = createChart(dataset)
+		val chart = createChart(data)
 		val chartPanel = ChartPanel(chart)
 		chartPanel.preferredSize = java.awt.Dimension(1200, 640)
 		contentPane = chartPanel
 	}
 
-	private fun createDataset(data: List<Pair<Double,Double>>): XYDataset {
-
-		val series1 = XYSeries("First")
-		for (p in data) {
-			series1.add(p.first, p.second)
-		}
-
+	private fun createChart(data: List<DatasetHolder>): JFreeChart {
 
 		val dataset = XYSeriesCollection()
+		data.forEachIndexed { i, datasetHolder ->
+			val series1 = XYSeries(datasetHolder.name)
+			for (p in datasetHolder.data) {
+			series1.add(p.first, p.second)
+		}
 		dataset.addSeries(series1)
-		return dataset
 	}
-
-	private fun createChart(dataset: XYDataset): JFreeChart {
 
 		// create the chart...
 		val chart = ChartFactory.createXYLineChart(
@@ -69,7 +61,7 @@ class GraphDrawer(title: String, data: List<Pair<Double,Double>>) : ApplicationF
 		// get a reference to the plot for further customisation...
 		val plot = chart.xyPlot
 
-
+//		plot.setDataset()
 
 
 		plot.backgroundPaint = Color.lightGray
@@ -82,10 +74,13 @@ class GraphDrawer(title: String, data: List<Pair<Double,Double>>) : ApplicationF
 		//		renderer.setSeriesShapesVisible(1, false);
 		val size = 3.0
 		val shape = Ellipse2D.Double(-size/2, -size/2, size, size)
-		renderer.baseShape = shape
-		renderer.basePaint = Color.red
-//		renderer.setLinesVisible(false)
-		renderer.setSeriesShape(0,shape)
+//		renderer.baseShape = shape
+//		renderer.basePaint = Color.red
+		for (i in 0..data.size-1) {
+			renderer.setSeriesShape(i, shape)
+			renderer.setSeriesPaint(i, data[i].paint)
+			renderer.setSeriesLinesVisible(i, data[i].line)
+		}
 		plot.renderer = renderer
 
 		// change the auto tick unit selection to integer units only...
@@ -103,12 +98,13 @@ class GraphDrawer(title: String, data: List<Pair<Double,Double>>) : ApplicationF
 	}
 
 	companion object {
-		fun draw(data: List<Pair<Double,Double>>) {
+		fun draw(data: List<DatasetHolder>) {
 			val demo = GraphDrawer("Line Chart Demo 6", data)
 			demo.pack()
 			RefineryUtilities.centerFrameOnScreen(demo)
 			demo.isVisible = true
 		}
 	}
-
 }
+
+class DatasetHolder(val name: String, val data: List<Pair<Double, Double>>, val paint: Color, val line: Boolean)
